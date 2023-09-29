adeaaef4b4f47e21ab7a689065f1c50df29ba5cc
Gianmaria Del Monte <39946305+gmgigi96@users.noreply.github.com>
2023-09-29 14:31:44 +0200

    Update cernbox.go

--- cernbox.go
+++ cernbox.go
@@ -3,12 +3,8 @@ package cernbox
 import (
 	// Add here all the plugins used by cernbox.
 	_ "github.com/cernbox/reva-plugins/eosprojects"
-	_ "github.com/cernbox/reva-plugins/favorite"
 	_ "github.com/cernbox/reva-plugins/group"
 	_ "github.com/cernbox/reva-plugins/otg"
-	_ "github.com/cernbox/reva-plugins/preferences"
-	_ "github.com/cernbox/reva-plugins/publicshare"
-	_ "github.com/cernbox/reva-plugins/share"
 	_ "github.com/cernbox/reva-plugins/storage/eoshomewrapper"
 	_ "github.com/cernbox/reva-plugins/storage/eoswrapper"
 	_ "github.com/cernbox/reva-plugins/thumbnails"
