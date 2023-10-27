eb4f54f1e6a3dd02b4c33de702607f4622896fbc
Gianmaria Del Monte <g.macmount@gmail.com>
2023-10-27 15:25:51 +0200

    include sql driver for shares in cernbox pkg

--- cernbox.go
+++ cernbox.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/cernbox/reva-plugins/eosprojects"
 	_ "github.com/cernbox/reva-plugins/group"
 	_ "github.com/cernbox/reva-plugins/otg"
+	_ "github.com/cernbox/reva-plugins/share/sql"
 	_ "github.com/cernbox/reva-plugins/storage/eoshomewrapper"
 	_ "github.com/cernbox/reva-plugins/storage/eoswrapper"
 	_ "github.com/cernbox/reva-plugins/thumbnails"
