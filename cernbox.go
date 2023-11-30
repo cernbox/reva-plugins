edae0384b5e2e3502764d7b942e5aa6e3bba3c61
Gianmaria Del Monte <g.macmount@gmail.com>
2023-11-30 10:54:00 +0100

    include cback in the cernbox build

--- cernbox.go
+++ cernbox.go
@@ -2,6 +2,8 @@ package cernbox
 
 import (
 	// Add here all the plugins used by cernbox.
+	_ "github.com/cernbox/reva-plugins/cback/http"
+	_ "github.com/cernbox/reva-plugins/cback/storage"
 	_ "github.com/cernbox/reva-plugins/cernboxspaces"
 	_ "github.com/cernbox/reva-plugins/eosprojects"
 	_ "github.com/cernbox/reva-plugins/group"
