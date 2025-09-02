0995e346fbb55489c2f91f538005703cdc0953b7
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2025-09-02 14:16:33 +0200

    Cleanup: removed plugins obsoleted by Spaces

--- cernbox.go
+++ cernbox.go
@@ -4,8 +4,6 @@ import (
 	// Add here all the plugins used by cernbox.
 	_ "github.com/cernbox/reva-plugins/cback/http"
 	_ "github.com/cernbox/reva-plugins/cback/storage"
-	_ "github.com/cernbox/reva-plugins/cernboxspaces"
-	_ "github.com/cernbox/reva-plugins/eosprojects"
 	_ "github.com/cernbox/reva-plugins/group"
 	_ "github.com/cernbox/reva-plugins/otg"
 	_ "github.com/cernbox/reva-plugins/share/sql"
